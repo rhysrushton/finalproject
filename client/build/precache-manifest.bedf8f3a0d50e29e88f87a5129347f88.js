79d20e895ae06ba0b797fc97862070dfe58d5ceb
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-23 19:50:50 +1000

    changes

---
+++ client/build/precache-manifest.bedf8f3a0d50e29e88f87a5129347f88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "deff30d2523d26123ff3",
+    "url": "/static/css/main.4a92f480.chunk.css"
+  },
+  {
+    "revision": "deff30d2523d26123ff3",
+    "url": "/static/js/main.644a40ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "b3463ccf0200b548968859c976e4fa68",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/finalproject");
 
+
+if (process.env.NODE_ENV === 'production'){
+   app.use(express.static('client/build'));
+}
+
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
